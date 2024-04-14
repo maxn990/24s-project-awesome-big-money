287422a014e4352ce8cc9ea575b36aa8f0a10ed5
Aryan J <aryan.km.jain@gmail.com>
2024-04-14 18:58:46 -0400

    -Aryan

---
+++ flask-app/src/leagueFans/leagueFans.py
@@ -0,0 +1,24 @@
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from src import db
+
+@leagues.route('/leagues', methods=['GET'])
+def get_leagueFans(league_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('''
+        SELECT fan.* 
+        FROM fan
+        JOIN leagues ON fan.fan_id = leagues.fan_id
+        WHERE leagues.league_id = %s
+    ''', (league_id,))
+    
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    the_response.mimetype = 'application/json'
+    return the_response
